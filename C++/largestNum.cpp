577eb453d6af9dd9773ac01670a5c778fe233848
Alex <alex.o.keyes@gmail.com>
2016-04-22 13:46:33 -0400

    add largestNum.cpp

---
+++ C++/largestNum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  static bool compareNum(string a, string b) {
+    return a + b > b + a;
+  }
+
+  string largestNumber(const vector<int> &num) {
+    string result;
+    vector<string> str;
+    for (int i = 0; i < num.size(); i++) {
+      str.push_back(to_string(num[i]));
+    }
+    sort(str.begin(), str.end(), compareNum);
+    for (int i = 0; i < str.size(); i++) {
+      result += str[i];
+    }
+
+    int pos = 0;
+    while (result[pos] == '0' && pos + 1 < result.size()) pos++; 
+    return result.substr(pos);
+  }
+};
