2871996a872bb81d3fa59dc165699dc3ec4155a2
Alex Keyes <alex.o.keyes@gmail.com>
2016-08-08 13:01:33 -0400

    Create uniquePaths.py

---
+++ Python/uniquePaths.py
@@ -0,0 +1,6 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def uniquePaths(self, A, B):
+        return math.factorial(A+B-2)/(math.factorial(A-1)*math.factorial(B-1))
