7ecc4ad9c96aa1375f578eab1c690f65317b9f30
Alex Keyes <alex.o.keyes@gmail.com>
2016-08-08 12:44:53 -0400

    Create isPalindrome.cpp

---
+++ C++/isPalindrome.cpp
@@ -0,0 +1,24 @@
+bool Solution::isPalindrome(int A) {
+     if(A<0)
+        return false;
+    int tenPow[10]={1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+    int i=0;
+    int noOfDigits=0;
+    while(i<10 && A/tenPow[i]>0) i++;
+    noOfDigits=i;
+    if(noOfDigits==1)
+        return true;
+    int B=A;
+    int C=0;
+    int temp;
+    for(i=0;i<noOfDigits;i++){
+        temp=B%10;
+        B/=10;
+        C+=temp*tenPow[noOfDigits-1-i];
+    }
+    if(A==C)
+        return true;
+    return false;
+    
+    
+}
