8a5847bc556233949a36a459f37f1047302cba41
Alex Keyes <alex.o.keyes@gmail.com>
2016-08-08 13:01:09 -0400

    Create uniquePaths.cpp

---
+++ C++/uniquePaths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        int uniquePaths(int m, int n) {
+            // m+n-2 C n-1 = (m+n-2)! / (n-1)! (m-1)! 
+            long long ans = 1;
+            for (int i = n; i < (m + n - 1); i++) {
+                ans *= i;
+                ans /= (i - n + 1);
+            }
+            return (int)ans;
+        }
+};
